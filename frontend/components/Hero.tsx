cd2c28c3149910d4b294d1aaa221fd6d2e0269c5
mfauzann982 <mfauzann982@gmail.com>
2024-09-26 20:16:45 +0700

    update hero

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -1,15 +1,13 @@
-import Image from 'next/image';
+import ProductCarousel from './ProductCarousel';
 
 const Hero = () => {
   return (
-    <div>
-      <Image
-        src="/hero.png"
-        alt="Hero"
-        width={720}
-        height={720}
-        className="object-contain m-auto my-20"
-      />
+    <div className="flex flex-col justify-center items-center w-[90%] max-w-screen-xl m-auto px-5">
+      <ProductCarousel productType="iPhone" imageSource="/iphone_hero.png" />
+      <ProductCarousel productType="iPad" imageSource="/ipad.jpg" />
+      <ProductCarousel productType="Macbook" imageSource="/macbook.jpg" />
+      <ProductCarousel productType="Airpods" imageSource="/airpods.png" />
+      <ProductCarousel productType="Watch" imageSource="/watch.png" />
     </div>
   );
 };
---
+++ frontend/components/ProductCarousel.tsx
@@ -0,0 +1,78 @@
+import Image from 'next/image';
+import { Button } from './ui/button';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from './ui/carousel';
+
+interface ProductCarouselProps {
+  productType: string;
+  imageSource: string;
+}
+
+const ProductCarousel = ({
+  productType,
+  imageSource,
+}: ProductCarouselProps) => {
+  return (
+    <div className="flex flex-col items-center w-full mt-20">
+      <div className="flex justify-between w-full items-center">
+        <h1 className="text-6xl font-bold">{productType}</h1>
+        <Button variant={'outline'}>See More</Button>
+      </div>
+
+      <div className="py-5">
+        <Image
+          src={imageSource}
+          alt="iPhone"
+          className="w-full h-auto max-h-[400px] md:max-h-screen rounded-lg"
+          width={1920}
+          height={720}
+        />
+      </div>
+
+      <div className="w-[90%] ">
+        <Carousel>
+          {/* <CarouselContent>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex aspect-square items-center justify-center p-6">
+                        <span className="text-3xl font-semibold">
+                          iPhone {index + 1}
+                        </span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent> */}
+          <CarouselContent>
+            {Array.from({ length: 5 }).map(() => (
+              <CarouselItem className="md:basis-1/3 lg:basis-1/4 flex flex-col items-center justify-center cursor-pointer">
+                <div className="p-1">
+                  <Image
+                    src={'/iphone.png'}
+                    alt="Iphone"
+                    width={120}
+                    height={150}
+                  />
+                  <p className="py-2">iPhone 15 Pro Max</p>
+                  <p className="font-bold">IDR 14,000,000</p>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCarousel;
