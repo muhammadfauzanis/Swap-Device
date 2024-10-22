f11513266a09a39ce261a9972d52400c044a7a6a
mfauzann982 <mfauzann982@gmail.com>
2024-10-22 19:09:18 +0700

    init prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
+// import bodyParser from 'body-parser';
+// import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(cookieParser());
+// app.use(bodyParser.json());
+// app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
