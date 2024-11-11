3a8601625a010f209b132b4fb3c9212a57a5f872
mfauzann982 <mfauzann982@gmail.com>
2024-11-11 09:34:27 +0700

    init login with google

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import apiDocumentation from './apiDocs.json';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import userRoutes from './routes/user';
+import { google } from 'googleapis';
 
 dotenv.config();
 
@@ -13,6 +14,23 @@ const app = express();
 
 // API Documentation with Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(apiDocumentation));
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  'http://localhost:5000/api/auth/google/callback'
+);
+
+const scopes = [
+  'https://www.googleapis.com/auth/userinfo.email',
+  'https://www.googleapis.com/auth/userinfo.profile',
+];
+
+const authorizationUrl = oauth2Client.generateAuthUrl({
+  access_type: 'offline',
+  scope: scopes,
+  include_granted_scopes: true,
+});
 
 app.use(cors());
 app.use(bodyParser.json());
