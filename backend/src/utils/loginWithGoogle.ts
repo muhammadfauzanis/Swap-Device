c35318198bd82a3e1b6d777c5396f9395062b1ba
mfauzann982 <mfauzann982@gmail.com>
2024-11-13 08:38:44 +0700

    fixing error

--- backend/src/utils/loginWithGoogle.ts
+++ backend/src/utils/loginWithGoogle.ts
@@ -3,7 +3,7 @@ import { google } from 'googleapis';
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  'http://localhost:5000/api/auth/google/callback'
+  process.env.GOOGLE_CALLBACK_URL
 );
 
 const scopes = [
